8fc011b9601f55d8bb951ee5192319baecd00b0f
pbaron <pebaron88@gmail.com>
2019-06-17 03:44:32 -0400

    done

---
+++ app/data/friends.js
@@ -0,0 +1,80 @@
+var friends = [
+
+  {
+    name: "The friend you want",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "3",
+      "1",
+      "5",
+      "1",
+      "4",
+      "3",
+      "1"
+    ]
+  },
+  {
+    name: "Sorry, no one is good enough for you",
+    scores: [
+      "3",
+      "1",
+      "5",
+      "4",
+      "1",
+      "5",
+      "1",
+      "1",
+      "5",
+      "3"
+    ]
+  },
+  {
+    name: "Jimmy Buffet",
+    score: [
+      "3",
+      "1",
+      "4",
+      "1",
+      "4",
+      "3",
+      "5",
+      "1",
+      "5",
+      "4"
+    ]
+  },
+  {
+    name: "Gizmo the Gremlin",
+    score: [
+      "2",
+      "4",
+      "3",
+      "1",
+      "4",
+      "3",
+      "5",
+      "1",
+      "5",
+      "4"
+    ]
+  },
+  {
+    name: "Jimmy Buffet",
+    score: [
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+  }
+];
+
+module.exports = friends;
